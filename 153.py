b395f85519455a63ce2c19173b667b5dad43dcf9
Henry Jacobs <henryjacobscode@gmail.com>
2023-10-16 15:56:37 -0400

    neetcode drama

--- 153.py
+++ 153.py
@@ -1,21 +1,45 @@
+# def findMin(nums):
+#     res = nums[0]
+#     l = 0 
+#     r = len(nums) - 1 
+
+#     while l <= r : 
+#         if nums[l] < nums[r]:
+#             res = min(res, nums[l])
+#             break 
+        
+#         m = (l + r) // 2 
+#         res = min(res, nums[m])
+#         if nums[m] >= nums[l]:
+#             l = m + 1 
+#         else: 
+#             r = m - 1     
+
 def findMin(nums):
     res = nums[0]
-    l = 0 
-    r = len(nums) - 1 
-
-    while l <= r : 
-        if nums[l] < nums[r]:
-            res = min(res, nums[l])
+    left = 0 
+    right = len(nums) - 1 
+    while left <= right:
+        if nums[left] < nums[right] : 
+            res = min(nums[left], res)
             break 
-        
-        m = (l + r) // 2 
-        res = min(res, nums[m])
-        if nums[m] >= nums[l]:
-            l = m + 1 
-        else: 
-            r = m - 1     
 
-nums = [3,4,5,1,2]
+        middle = (right + left) // 2 
+        res = min(res, nums[middle])
+        if nums[middle] >= nums[left] :
+            left = middle + 1
+        else:
+            right = middle - 1 
+
+
+    return res 
+
+
+
+
+
+# nums = [3,4,5,1,2]
+nums = [3, 1, 2]
 nums2 = [7, 0, 1, 2, 4, 5, 6]
 nums3 = [11, 13, 15, 17]
 findMin(nums)
--- 875.py
+++ 875.py
@@ -1,23 +1,59 @@
+# def minEatingSpeed(piles, h):
+#     import math 
+#     l, r = 1, max(piles)
+#     res = r 
+
+#     while l <= r :
+#         middle = (l + r) // 2 
+#         hours = 0 
+#         for p in piles:
+#             hours += math.ceil(p / middle)
+
+#         if hours <= h :
+#             res = min(res, middle)
+#             r = middle - 1 
+#         else :
+#             l = middle + 1 
+    
+#     return res 
+
+
 def minEatingSpeed(piles, h):
     import math 
-    l, r = 1, max(piles)
-    res = r 
+    ## We know that the maximum is going to be the largest number 
+    ## bananas in the given piles. The lowest possible amount is going to be 
+    ## 1
+    maxNum = max(piles)
+    if len(piles) == h :
+        return maxNum 
+    left, right = 1, maxNum 
+    while left <= right :
+        middle = (left + right) // 2 
+        totalTime = 0
+        for pile in piles:
+            totalTime += math.ceil(pile/middle)
+        if totalTime < h :
+            right = middle - 1 
+        elif totalTime > h :
+            left = middle + 1 
+        else:
+            return middle 
 
-    while l <= r :
-        middle = (l + r) // 2 
-        hours = 0 
-        for p in piles:
-            hours += math.ceil(p / middle)
 
-        if hours <= h :
-            res = min(res, middle)
-            r = middle - 1 
-        else :
-            l = middle + 1 
-    
-    return res 
-
-
-piles = [30,11,23,4,20]
-hours = 6
+# piles = [30,11,23,4,20]
+# hours = 6
+# piles = [3,6,7,11]
+# hours = 8 
+piles = [312884470]
+hours = 312884469
 minEatingSpeed(piles, hours)
+
+
+'''
+
+[332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
+h =
+823855818
+
+
+'''
